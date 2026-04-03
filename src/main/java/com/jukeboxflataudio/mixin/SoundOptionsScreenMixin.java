ee2d49908322bb51b7efc41142126e9a25497503
sub2jam <jamiewurth1234@gmail.com>
2026-04-03 22:27:27 +0200

    SoundOptionsScreenMixin.java

--- src/main/java/com/jukeboxflataudio/mixin/SoundOptionsScreenMixin.java
+++ src/main/java/com/jukeboxflataudio/mixin/SoundOptionsScreenMixin.java
@@ -3,7 +3,6 @@ package com.jukeboxflataudio.mixin;
 import com.jukeboxflataudio.JukeboxFlatAudioMod;
 import net.minecraft.client.gui.screen.option.SoundOptionsScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
-import net.minecraft.client.option.GameOptions;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
@@ -11,25 +10,17 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-/**
- * Adds a "Flat Jukebox Audio: ON/OFF" toggle button to the Sound Options screen,
- * sitting just below the existing volume sliders.
- */
 @Mixin(SoundOptionsScreen.class)
-public class SoundOptionsScreenMixin extends Screen {
+public abstract class SoundOptionsScreenMixin extends Screen {
 
     protected SoundOptionsScreenMixin() {
         super(Text.empty());
     }
 
-    @Inject(method = "init()V", at = @At("TAIL"))
+    @Inject(method = "init", at = @At("TAIL"))
     private void jukeboxFlatAudio_addToggle(CallbackInfo ci) {
-        SoundOptionsScreen self = (SoundOptionsScreen) (Object) this;
-
-        // Place the button at the bottom of the option list area, centred
         int buttonWidth = 200;
         int buttonX = this.width / 2 - buttonWidth / 2;
-        // Position it near the bottom of the screen, above the Done button
         int buttonY = this.height - 48;
 
         this.addDrawableChild(
@@ -47,7 +38,6 @@ public class SoundOptionsScreenMixin extends Screen {
     }
 
     private static Text getToggleText() {
-        String state = JukeboxFlatAudioMod.flatJukeboxAudio ? "ON" : "OFF";
-        return Text.literal("Flat Jukebox Audio: " + state);
+        return Text.literal("Flat Jukebox Audio: " + (JukeboxFlatAudioMod.flatJukeboxAudio ? "ON" : "OFF"));
     }
 }
