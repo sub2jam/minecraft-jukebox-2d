941fed06226deb6545acd8ff7c145365d01be075
sub2jam <jamiewurth1234@gmail.com>
2026-04-03 22:51:10 +0200

    JukeboxBlockEntityMixin.java

--- src/main/java/com/jukeboxflataudio/mixin/JukeboxBlockEntityMixin.java
+++ src/main/java/com/jukeboxflataudio/mixin/JukeboxBlockEntityMixin.java
@@ -3,9 +3,7 @@ package com.jukeboxflataudio.mixin;
 import com.jukeboxflataudio.JukeboxFlatAudioMod;
 import net.minecraft.client.sound.SoundInstance;
 import net.minecraft.client.sound.SoundManager;
-import net.minecraft.client.sound.PositionedSoundInstance;
 import net.minecraft.sound.SoundCategory;
-import net.minecraft.util.math.random.Random;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -20,22 +18,6 @@ public class JukeboxBlockEntityMixin {
         if (sound.getCategory() != SoundCategory.RECORDS) return;
         if (sound.getAttenuationType() == SoundInstance.AttenuationType.NONE) return;
 
-        ci.cancel();
-
-        SoundInstance flat = new PositionedSoundInstance(
-                sound.getId(),
-                SoundCategory.RECORDS,
-                sound.getVolume(),
-                sound.getPitch(),
-                Random.create(),
-                false,
-                0,
-                SoundInstance.AttenuationType.NONE,
-                0, 0, 0,
-                true
-        );
-
-        // Use playImmediately to bypass server communication entirely
-        ((SoundManager)(Object)this).play(flat);
+        ci.setReturnValue(null);
     }
 }
