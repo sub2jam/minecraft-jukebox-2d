dce2a3a43c236fd29534f31d5b5c39eeb3ec9aea
sub2jam <jamiewurth1234@gmail.com>
2026-04-03 23:09:38 +0200

    Delete src/main/java/com/jukeboxflataudio/mixin/SoundOptionsScreenMixin.java

--- src/main/java/com/jukeboxflataudio/mixin/SoundOptionsScreenMixin.java
+++
@@ -1,8 +0,0 @@
-package com.jukeboxflataudio.mixin;
-
-import org.spongepowered.asm.mixin.Mixin;
-import net.minecraft.client.gui.screen.option.SoundOptionsScreen;
-
-@Mixin(SoundOptionsScreen.class)
-public class SoundOptionsScreenMixin {
-}
