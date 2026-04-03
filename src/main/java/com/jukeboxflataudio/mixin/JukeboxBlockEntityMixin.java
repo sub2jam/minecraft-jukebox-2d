56ae4fca5fca5c5bc45584a2123593aa02a741c0
sub2jam <jamiewurth1234@gmail.com>
2026-04-03 23:26:49 +0200

    JukeboxBlockEntityMixin.java

--- src/main/java/com/jukeboxflataudio/mixin/JukeboxBlockEntityMixin.java
+++ src/main/java/com/jukeboxflataudio/mixin/JukeboxBlockEntityMixin.java
@@ -1,8 +1,6 @@
 package com.jukeboxflataudio.mixin;
 
-import com.jukeboxflataudio.JukeboxFlatAudioMod;
 import net.minecraft.client.sound.AbstractSoundInstance;
-import net.minecraft.client.sound.SoundInstance;
 import net.minecraft.sound.SoundCategory;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -12,11 +10,11 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(AbstractSoundInstance.class)
 public class JukeboxBlockEntityMixin {
 
-    @Inject(method = "getAttenuationType", at = @At("HEAD"), cancellable = true)
-    private void jukeboxFlatAudio_flatten(CallbackInfoReturnable<SoundInstance.AttenuationType> ci) {
+    @Inject(method = "isRelative", at = @At("RETURN"), cancellable = true)
+    private void flattenJukeboxPanning(CallbackInfoReturnable<Boolean> ci) {
         AbstractSoundInstance self = (AbstractSoundInstance)(Object)this;
         if (self.getCategory() == SoundCategory.RECORDS) {
-            ci.setReturnValue(SoundInstance.AttenuationType.NONE);
+            ci.setReturnValue(true);
         }
     }
 }
