8003390181135eee61fe09a1b0ec5a28d95bce6e
sub2jam <jamiewurth1234@gmail.com>
2026-04-03 22:22:40 +0200

    JukeboxBlockEntityMixin.java

--- src/main/java/com/jukeboxflataudio/mixin/JukeboxBlockEntityMixin.java
+++ src/main/java/com/jukeboxflataudio/mixin/JukeboxBlockEntityMixin.java
@@ -9,13 +9,13 @@ import net.minecraft.util.math.random.Random;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(SoundManager.class)
 public class JukeboxBlockEntityMixin {
 
     @Inject(method = "play", at = @At("HEAD"), cancellable = true)
-    private void jukeboxFlatAudio_interceptPlay(SoundInstance sound, CallbackInfo ci) {
+    private void jukeboxFlatAudio_interceptPlay(SoundInstance sound, CallbackInfoReturnable<?> ci) {
         if (!JukeboxFlatAudioMod.flatJukeboxAudio) return;
         if (sound.getCategory() != SoundCategory.RECORDS) return;
         if (sound.getAttenuationType() == SoundInstance.AttenuationType.NONE) return;
